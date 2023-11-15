33da724b01cab18b3d4f12225fcc61d06922080f
Benjamin A <benjamin.alvarado1510@gmail.com>
2023-11-14 19:41:45 -0500

    azfasf

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Honorio Delgado Espinoza </h1>
+      <h2>Meredith Alanis Escudero Sucasaca</h2>
+      <h3>Noche VI Semestre</h3>
+      <h4>Computacion e Informatica</h4>
+    </div>
+  );
+}
+
+export default App;
